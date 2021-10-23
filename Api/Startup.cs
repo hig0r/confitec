d92508dd3fee41abe7067fac8ac35296c7dd96d1
higor <github@higor.ninja>
2021-10-23 18:42:57 -0300

    adicionando newtonsoftjson

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -13,6 +13,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 namespace Confitec
 {
@@ -28,7 +31,21 @@ namespace Confitec
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddControllers()
+                .AddNewtonsoftJson(
+                config =>
+                {
+                    config.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    config.SerializerSettings.Converters.Add(new StringEnumConverter
+                    {
+                        NamingStrategy = new KebabCaseNamingStrategy()
+                    });
+                    config.SerializerSettings.ContractResolver = new DefaultContractResolver
+                    {
+                        NamingStrategy = new CamelCaseNamingStrategy()
+                    };
+                });
 
             services.AddDbContext<ConfitecDbContext>();
         }
