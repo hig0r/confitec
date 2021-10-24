d74acf05e51cd95d9566a62e5aa5fdc58e2bb9b1
higor <github@higor.ninja>
2021-10-24 10:56:26 -0300

    rota e modulo de usuarios

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'usuarios' },
+  { path: 'usuarios', loadChildren: () => import('./usuarios/usuarios.module').then(x => x.UsuariosModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
